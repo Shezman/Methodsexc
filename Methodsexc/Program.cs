0dda20b47bece49a3d3289538881ceb0d7c6cfa6
Sherman Tanuwidjaja <shermantanuwidjaja@gmail.com>
2018-04-24 01:41:42 -0700

    Add project files.

---
+++ Methodsexc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodsexc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Integer do you want to do the math operation on?");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+           
+            userInt = methodClass.AddOperator(userInt);
+            userInt = methodClass.DivideOperator(userInt);
+            userInt = methodClass.SubtractOperator(userInt);
+
+            Console.WriteLine(userInt);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methodsexc/methodClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodsexc
+{
+
+    public class methodClass
+    {
+        public static int AddOperator(int x)
+        {
+            int result = x + 10;
+            return result;
+        }
+
+        public static int DivideOperator(int x)
+        {
+            int result = x / 2;
+            return result;
+        }
+        public static int SubtractOperator(int x)
+        {
+            int result = x - 1;
+            return result;
+        }
+    }
+
+   
+}
